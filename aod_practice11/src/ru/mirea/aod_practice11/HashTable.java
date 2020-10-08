932417a7e02eff2b720323938bf4a16d24d155d5
Anastasia <nst10knv@gmail.com>
2020-10-08 23:20:29 +0300

    change pr11

---
+++ aod_practice11/src/ru/mirea/aod_practice11/HashTable.java
@@ -0,0 +1,2 @@
+package ru.mirea.aod_practice11;public class HashTable {
+}
--- aod_practice11/src/ru/mirea/aod_practice11/Test.java
+++ aod_practice11/src/ru/mirea/aod_practice11/Test.java
@@ -3,6 +3,6 @@ package ru.mirea.aod_practice11;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
